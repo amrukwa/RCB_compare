c0180ba886a129e66d557bf79fdebc5d2f930bd3
amrukwa <a.mrukwa00@gmail.com>
2020-11-16 17:20:19 +0100

    Solve plot loading and displaying problems

--- server.R
+++ server.R
@@ -5,34 +5,35 @@ library(shinyalert)
 source('Calculate_TES.R')
 
 server <- function(input, output, session) {
-  v <- reactiveValues(used_cdata=NULL, used_edata=NULL, gen=NULL, res_TES=NULL)
+  v <- reactiveValues(gen=NULL, res_TES=NULL, calc=NULL)
+  tables <- reactiveValues(used_cdata=NULL, used_edata=NULL)
   cdata <- reactive({handle_file(input$control_file, input$control_name)})
   edata <- reactive({handle_file(input$exp_file, input$exp_name)})
   observeEvent(input$generate_plot, {
-    v$used_edata <- load_data(input$exemplary_files, edata(), "experimental",input$exp_name)
-    v$used_cdata <- load_data(input$exemplary_files, cdata(), "control",input$control_name)
+    tables$used_edata <- load_data(input$exemplary_files, edata(),input$exp_name, "experimental")
+    tables$used_cdata <- load_data(input$exemplary_files, cdata(),input$control_name, "control")
+    if(is.null(tables$used_cdata) | is.null(tables$used_edata) ){
+      v$calc = NULL}
     })
-  observeEvent(input$generate_plot, { v$res_TES <- calculate_TES(input$method, v$used_cdata, v$used_edata)
-    v$gen <- grid.arrange(v$res_TES[["Plot1"]],v$res_TES[["Plot2"]],nrow=2)} )
-  erow <- reactive({nrow(v$used_edata)})
-  crow <- reactive({nrow(v$used_cdata)})
-  observeEvent(c(v$used_edata, v$used_cdata), 
+  erow <- reactive({nrow(tables$used_edata)})
+  crow <- reactive({nrow(tables$used_cdata)})
+  observeEvent(c(tables$used_edata, tables$used_cdata), 
                {
-                 validate(need(v$used_edata, ''), need(v$used_cdata, ''))
-                 if(erow()==crow()) {if(all(v$used_cdata==v$used_edata)) {shinyalert("Warning","Uploaded files have the same content",
+                 validate(need(tables$used_edata, ''), need(tables$used_cdata, ''))
+                 v$calc = TRUE
+                 if(erow()==crow()) {if(all(tables$used_cdata==tables$used_edata)) {shinyalert("Warning","Uploaded files have the same content",
                                                                                      type="warning")}}
                })
-  
-  output$ccontents <- renderTable({v$used_cdata})
-  output$econtents <- renderTable({v$used_edata})
+  output$ccontents <- renderTable({tables$used_cdata})
+  output$econtents <- renderTable({tables$used_edata})
   output$ncontrol <- renderText({crow()})
   output$nexp <- renderText({erow()})
+  observeEvent(v$calc, {
+    v$res_TES <- calculate_TES(input$method, tables$used_cdata, tables$used_edata)
+    v$gen <- grid.arrange(v$res_TES[["Plot1"]],v$res_TES[["Plot2"]],nrow=2)} )
+  
   output$nexp1 <- renderPlot({
-    validate(
-      need(v$used_edata, 'Choose correct experimental treatment file.'),
-      need(v$used_cdata, 'Choose correct control treatment file.'),
-      need(input$generate_plot, "Click 'calculate' after updating files.")
-    )
+    validate(need(v$res_TES, " "))
     grid.arrange(v$res_TES[["Plot1"]],v$res_TES[["Plot2"]],nrow=2)})
   
   output$downloadData <- downloadHandler(
@@ -43,8 +44,8 @@ server <- function(input, output, session) {
       ggsave(v$gen, filename = file)
     }
     )
-  observeEvent(input$generate_plot, {
-    if (is.null(v$used_edata) || is.null(v$used_cdata))
+  observeEvent(v$res_TES, {
+    if (is.null(tables$used_edata) || is.null(tables$used_cdata))
       shinyjs::hide("downloadData")
     else
       shinyjs::show("downloadData")
