de097bf2ac3f80984bae5d25947eae06d71dd94a
amrukwa <a.mrukwa00@gamil.com>
2021-04-06 14:34:33 +0200

    Add RMST calculation

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ source('Calculate_TES.R')
 
 server <- function(input, output, session) {
   # prepare reactive values
-  v <- reactiveValues(gen=NULL, res_TES=NULL, method=NULL, to_save=NULL)
+  v <- reactiveValues(gen=NULL, res_TES=NULL, method=NULL, to_save=NULL, caption=NULL)
   tables <- reactiveValues(used_cdata=NULL, used_edata=NULL)
   # load the user files
   cdata <- reactive({handle_file(input$control_file, input$control_name)})
@@ -33,14 +33,26 @@ server <- function(input, output, session) {
     validate(need(tables$used_edata, "Specify the correct experimental treatment."), need(tables$used_cdata, "Specify the correct control treatment."))
     withBusyIndicatorServer("generate_plot",
     {v$res_TES <- calculate_TES(v$method, tables$used_cdata, tables$used_edata)
-    v$to_save <- grid.arrange(v$res_TES[["Plot1"]], 
-                          v$res_TES[["Plot2"]], 
-                          nrow=2)
-    v$gen <- grid.arrange(v$res_TES[["Plot1"]] + plot_theme, 
-                          v$res_TES[["Plot2"]] + plot_theme, 
-                      nrow=2)
+    if (v$res_TES[["TES"]][["TES"]] >0){
+      v$caption <- paste("RMST increase:", v$res_TES[["TES"]][["TES"]]*4.952, "month(s)", sep=" ")
+      v$gen <- grid.arrange(v$res_TES[["Plot1"]] + plot_theme, 
+                            v$res_TES[["Plot2"]] + plot_theme+ labs(caption=v$caption) +
+                              theme(plot.caption=element_text(size=18, hjust=0, margin=margin(15,15,0,0))), 
+                            nrow=2)
+      v$to_save <- grid.arrange(v$res_TES[["Plot1"]], 
+                                v$res_TES[["Plot2"]]+ labs(caption=v$caption) +
+                                  theme(plot.caption=element_text(size=18, hjust=0, margin=margin(15,15,0,0))), 
+                                nrow=2)
+    }else{
+      v$gen <- grid.arrange(v$res_TES[["Plot1"]] + plot_theme, 
+                            v$res_TES[["Plot2"]] + plot_theme, 
+                            nrow=2)
+      v$to_save <- grid.arrange(v$res_TES[["Plot1"]], 
+                                v$res_TES[["Plot2"]], 
+                                nrow=2)
+    }
     v$gen})
-    }, height=550, width=550, bg="transparent", execOnResize = TRUE)
+    }, height=565, width=550, bg="transparent", execOnResize = TRUE)
   # prepare plot for download
   output$downloadData1 <- downloadHandler(
     filename = function() {
